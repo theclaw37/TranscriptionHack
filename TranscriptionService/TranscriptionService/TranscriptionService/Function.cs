84fd15251b5dbb5372214d4a770e054381728787
wroclaw-agile <wroclaw-agile>
2017-04-06 22:37:29 +0100

    TranscriptionService basic implementation - without checking whether it's working on Amazon

--- TranscriptionService/TranscriptionService/TranscriptionService/Function.cs
+++ TranscriptionService/TranscriptionService/TranscriptionService/Function.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.IO.Compression;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -14,6 +14,10 @@ using Newtonsoft.Json;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.S3;
+using Amazon.Lambda.S3Events;
+using Amazon.S3.Model;
+using Newtonsoft.Json.Linq;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -22,7 +26,10 @@ namespace TranscriptionService
 {
     public class Functions
     {
+        private const string VOCI_TOKEN = "VociToken";
+
         IDynamoDBContext DDBContext { get; set; }
+        IAmazonS3 S3Client { get; set; }
 
         /// <summary>
         /// Default constructor that Lambda will invoke.
@@ -33,8 +40,51 @@ namespace TranscriptionService
 
             var config = new DynamoDBContextConfig { Conversion = DynamoDBEntryConversion.V2 };
             DDBContext = new DynamoDBContext(new AmazonDynamoDBClient(), config);
+            S3Client = new AmazonS3Client();
         }
 
+        /// <summary>
+        /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
+        /// to respond to S3 notifications.
+        /// </summary>
+        /// <param name="evnt"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task FunctionHandler(S3Event evnt, ILambdaContext context)
+        {
+            var s3Event = evnt.Records?[0].S3;
+            if (s3Event == null)
+            {
+                return;
+            }
+
+            try
+            {
+                var preSignedUrlRequest = new GetPreSignedUrlRequest
+                {
+                    BucketName = s3Event.Bucket.Name,
+                    Key = s3Event.Object.Key,
+                    Expires = DateTime.Now.AddHours(6)
+                };
+
+                var transcript = new Transcript
+                {
+                    CreatedOn = DateTime.Now,
+                    Id = s3Event.Object.Key,
+                    FileUrl = S3Client.GetPreSignedURL(preSignedUrlRequest)
+                };               
+
+                context.Logger.LogLine($"Saving transcript with id {transcript.Id}");
+                await DDBContext.SaveAsync(transcript);              
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
+                context.Logger.LogLine(e.Message);
+                context.Logger.LogLine(e.StackTrace);
+                throw;
+            }
+        }
 
         /// <summary>
         /// A Lambda function to respond to HTTP Get methods from API Gateway
@@ -57,7 +107,7 @@ namespace TranscriptionService
 
         public async Task<APIGatewayProxyResponse> ProcessTranscripts(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            context.Logger.LogLine("Getting blogs");
+            context.Logger.LogLine("Getting transcripts");
             var search = DDBContext.ScanAsync<Transcript>(new[] {new ScanCondition("VociRequestId", ScanOperator.IsNull)});
             var transcripts = await search.GetNextSetAsync();
             context.Logger.LogLine($"Found {transcripts.Count} transcripts");
@@ -66,7 +116,47 @@ namespace TranscriptionService
             {
                 await SendTranscriptToVoci(transcript);
             }
-            //Save transcripts in DynamoDb
+
+            await DDBContext.SaveAsync(transcripts);
+            
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = JsonConvert.SerializeObject(transcripts),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+
+            return response;
+        }
+
+        public async Task<APIGatewayProxyResponse> GetTranscripts(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var client = new HttpClient();
+
+            context.Logger.LogLine("Getting transcripts");
+            var search = DDBContext.ScanAsync<Transcript>(new[]
+            {
+                new ScanCondition("VociTranscript", ScanOperator.IsNull),
+                new ScanCondition("VociRequestId", ScanOperator.IsNotNull)
+            });
+
+            var transcripts = await search.GetNextSetAsync();
+            context.Logger.LogLine($"Found {transcripts.Count} transcripts");
+
+            var token = Environment.GetEnvironmentVariable(VOCI_TOKEN);
+
+            foreach (var transcript in transcripts)
+            {                
+                var urlTranscribeResult =
+                    $"https://vcloud.vocitec.com/transcribe/result?token={token}&requestid={transcript.VociRequestId}";
+                var transcribeResultResponse = await client.GetAsync(urlTranscribeResult);
+                if (transcribeResultResponse.IsSuccessStatusCode)
+                {
+                    transcript.VociTranscript = await transcribeResultResponse.Content.ReadAsStringAsync();
+                }                
+            }
+
+            await DDBContext.SaveAsync(transcripts);
 
             var response = new APIGatewayProxyResponse
             {
@@ -80,15 +170,23 @@ namespace TranscriptionService
 
         private async Task SendTranscriptToVoci(Transcript transcript)
         {
-            HttpClient client = new HttpClient();
+            var client = new HttpClient();
+            
+            var zipStream = new MemoryStream();
+            using (var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create))
+            {               
+                var zipEntry = zipArchive.CreateEntry(transcript.Id);
+                using (var zipEntryStream = zipEntry.Open())
+                using (var stream = await client.GetStreamAsync(transcript.FileUrl))
+                    await stream.CopyToAsync(zipEntryStream);                
+            }
 
             var url = @"https://vcloud.vocitec.com/transcribe";
 
-            var token = "";//get from env variables (you can find it in LastPass)
+            var token = Environment.GetEnvironmentVariable(VOCI_TOKEN);
             var tokenContent = new StringContent(token);
-
-            //get file from transcript.FileUrl in line below and make zip file
-            var fileContent = new StreamContent(new FileStream(@"C:\Temp\file.zip", FileMode.Open));
+            
+            var fileContent = new StreamContent(zipStream);
             fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/zip");
 
             var content = new MultipartFormDataContent
@@ -97,17 +195,10 @@ namespace TranscriptionService
                 {fileContent, "file", "file.zip"}
             };
 
-            var result = await client.PostAsync(url, content);
-            var response = await result.Content.ReadAsStringAsync();
-            //response is in format "{\"requestid\":\"0366be4a-bd34-429e-a69a-6cb1afb85c64\"}"
-            //save 
-        }
-
-        /* CODE FOR RETRIEVING TRANSCRIPT
-         * var requestId = "0366be4a-bd34-429e-a69a-6cb1afb85c64";
-           var urlResult =
-               $"https://vcloud.vocitec.com/transcribe/result?token={token}&requestid={requestId}";
-           var result2 = client.GetAsync(urlResult).Result;
-           Console.WriteLine(result2.Content.ReadAsStringAsync().Result);*/
+            var vociResponse = await client.PostAsync(url, content);
+            var json = JObject.Parse(await vociResponse.Content.ReadAsStringAsync());
+            transcript.VociRequestId = json.Value<string>("requestid");
+            transcript.RequestedOn = DateTime.Now;            
+        }              
     }
 }
--- TranscriptionService/TranscriptionService/TranscriptionService/Transcript.cs
+++ TranscriptionService/TranscriptionService/TranscriptionService/Transcript.cs
@@ -8,8 +8,9 @@ namespace TranscriptionService
     public class Transcript
     {
         public string Id { get; set; }
+        public DateTime CreatedOn { get; set; }
         public string FileUrl { get; set; }
-
+        public DateTime RequestedOn { get; set; }
         public string VociRequestId { get; set; }
         public string VociTranscript { get; set; }
     }
